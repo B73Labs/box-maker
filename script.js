4b84c5a1be9ebb020976a4255c81f167d423db56
Michael Best <mbest@arcticlinux.com>
2022-09-30 18:01:51 -0600

    - add package.json - add svg.js - add svg.path.js - initial outline implementation using svg.js and svg.path.js

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ let offset = 20; // used for window size
 
 let scale = 72; // scale to export correctly for inches
 
-let box=$('.box');
+let box = $('#rectbox');
 
-function generateBox() {
+function generateBox()
+{
     let widthIn = parseFloat($('#width').val());
     let heightIn = parseFloat($('#height').val());
     let depthIn = parseFloat($('#depth').val());
 
-    let fullWidthIn = depthIn / 2 + widthIn * 2 + depthIn * 2;
-    let fullHeightIn = depthIn + heightIn + depthIn + offset * 0.8 / scale * 2;
+    let fullWidthIn = depthIn / 2 + widthIn * 2 + depthIn * 2 + 'in';
+    let fullHeightIn = depthIn + heightIn + depthIn + offset * 0.8 / scale * 2 + 'in';
 
     console.log(`fullWidthIn: ${fullWidthIn} fullHeightIn ${fullHeightIn}`);
 
@@ -30,62 +31,80 @@ function generateBox() {
     viewBoxHeight = (depth + height + depth) * 1.3;// without 1.3 scale factor, the svg preview is getting cut off
 
     console.log(`generateBox with width ${width} height ${height} depth ${depth} viewBoxWidth ${viewBoxWidth} viewBoxHeight ${viewBoxHeight}`);
-    let outline;
-    outline += `
-M${depth / 2 + width / 10} ${depth / 2 - offset * 0.8}
-l-${width / 10} ${offset * 0.8}
-    `;
-    outline = `
-M${depth / 2} ${depth / 2}
-v${depth} 
-h-${depth / 2}
-v${height} 
-h${depth / 2} 
-v${depth}
-l${width / 10} ${offset * 0.8}
-h${width - width / 10 * 2}
-l${width / 10} -${offset * 0.8}
-v-${depth} 
-M${width + depth / 2} ${depth / 2 + depth + height + width / 2}
-h${depth} 
-v-${width / 2} 
-h${width} 
-v${width / 2} 
-h${depth} 
-v-${width / 2} 
-v-${height} 
-v-${width / 2} 
-h-${depth} 
-v${width / 2} 
-h-${width} 
-v-${width / 2} 
-h-${depth} 
-v${width / 2} 
-M${depth / 2 + width} ${depth / 2 + depth - width / 2} 
-v-${depth - width / 2}
-l-${width / 10} -${offset * 0.8}
-h-${width - width / 10 * 2}`
-    return `<svg width="${fullWidthIn}in" height="${fullHeightIn}in" viewBox="0  0 ${viewBoxWidth + paddingAmt} ${viewBoxHeight + paddingAmt}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none"> 
-    <g class="cuts" stroke="blue">
-        <path class="cut" vector-effect="non-scaling-stroke" d="${outline}"/>
-    </g>
-    <g stroke="black" stroke-weight="1" class="score" stroke-dasharray="2">
-      <path class="score" vector-effect="non-scaling-stroke" d="M${depth / 2} ${depth / 2} h${width} 
-M${depth / 2} ${depth / 2 + depth}h${width} 
-M${depth / 2} ${depth / 2 + depth + height}h${width} 
-M${depth / 2} ${depth / 2 + depth + height + depth}h${width} 
-M${depth / 2} ${depth / 2 + depth}v${height} 
-M${depth / 2 + width} ${depth / 2 + depth}v${height} 
-M${depth / 2 + width} ${depth / 2 + depth}h${depth} 
-M${depth / 2 + width} ${depth / 2 + depth + height}h${depth} 
-M${depth / 2 + width + depth} ${depth / 2 + depth}v${height} M${depth / 2 + width + depth + width} ${depth / 2 + depth}v${height} 
-M${depth / 2 + width + depth + width} ${depth / 2 + depth}h${depth} M${depth / 2 + width + depth + width} ${depth / 2 + depth + height}h${depth}
-"/>
-    </g>
-  </svg>`;
+    let draw = SVG().addTo('#rectbox');
+    draw
+        .size(fullWidthIn, fullHeightIn)
+        .path()
+        .attr({
+            fill: 'none', stroke: '#0000FF', 'stroke-width': 1
+        })
+
+        .M(depth / 2 + width / 10, depth / 2 - offset * 0.8)
+        .l(-width / 10, offset * 0.8)
+        .M(depth / 2, depth / 2)
+        .v(depth)
+        .h(-depth / 2)
+        .v(height)
+        .h(depth / 2)
+        .v(depth)
+        .l(width / 10, offset * 0.8)
+        .h(width - width / 10 * 2)
+        .l(width / 10, -offset * 0.8)
+        .v(-depth)
+        .M(width + depth / 2, depth / 2 + depth + height + width / 2)
+        .h(depth)
+        .v(-width / 2)
+        .h(width)
+        .v(width / 2)
+        .h(depth)
+        .v(-width / 2)
+        .v(-height)
+        .v(-width / 2)
+        .h(-depth)
+        .v(width / 2)
+        .h(-width)
+        .v(-width / 2)
+        .h(-depth)
+        .v(width / 2)
+        .M(depth / 2 + width, depth / 2 + depth - width / 2)
+        .v(-(depth - width / 2))
+        .l(-width / 10, -offset * 0.8)
+        .h(-(width - (width / 10 * 2)));
+
+    draw
+        .size(fullWidthIn, fullHeightIn)
+        .path()
+        .attr({
+            fill: 'none', stroke: '#000', 'stroke-width': 1, 'stroke-dasharray': 2
+        })
+        .M(depth / 2, depth / 2)
+        .h(width)
+        .M(depth / 2, depth / 2 + depth)
+        .h(width)
+        .M(depth / 2, depth / 2 + depth + height)
+        .h(width)
+        .M(depth / 2, depth / 2 + depth + height + depth)
+        .h(width)
+        .M(depth / 2, depth / 2 + depth)
+        .v(height)
+        .M(depth / 2 + width, depth / 2 + depth)
+        .v(height)
+        .M(depth / 2 + width, depth / 2 + depth)
+        .h(depth)
+        .M(depth / 2 + width, depth / 2 + depth + height)
+        .h(depth)
+        .M(depth / 2 + width + depth, depth / 2 + depth)
+        .v(height)
+        .M(depth / 2 + width + depth + width, depth / 2 + depth)
+        .v(height)
+        .M(depth / 2 + width + depth + width, depth / 2 + depth)
+        .h(depth)
+        .M(depth / 2 + width + depth + width, depth / 2 + depth + height)
+        .h(depth);
 }
 
-function addWindow(type) {
+function addWindow(type)
+{
     let windowPath;
     width = parseFloat($('#width').val()) * scale;
     height = parseFloat($('#height').val()) * scale;
@@ -93,7 +112,8 @@ function addWindow(type) {
 
     let ns = 'http://www.w3.org/2000/svg';
 
-    if (type === "rect") {
+    if(type === "rect")
+    {
         windowPath = document.createElementNS(ns, 'rect');
         windowPath.setAttribute('class', "window cut");
         windowPath.setAttribute("stroke-width", '1');
@@ -104,7 +124,9 @@ function addWindow(type) {
         windowPath.setAttribute("height", `${height - offset * 2}`);
         $('svg .cuts .window').remove();
         $('svg .cuts').append(windowPath);
-    } else if (type === "ellipse") {
+    }
+    else if(type === "ellipse")
+    {
         windowPath = document.createElementNS(ns, 'ellipse');
         windowPath.setAttribute('class', 'window cut');
         windowPath.setAttribute("stroke-width", '1');
@@ -115,17 +137,20 @@ function addWindow(type) {
         windowPath.setAttribute("ry", `${(height - offset * 2) / 2}`);
         $('svg .cuts .window').remove();
         $('svg .cuts').append(windowPath);
-    } else {
+    }
+    else
+    {
         $('svg .cuts .window').remove();
     }
-    let newSVG = (new XMLSerializer).serializeToString($('.box svg')[0]);
+    let newSVG = (new XMLSerializer).serializeToString($('#box svg')[0]);
     console.log(`newSVG: ${newSVG}`);
     updateBox(newSVG);
 }
 
 // setActive
 // add active class to selected window type
-function setActive(el) {
+function setActive(el)
+{
     let type = $(el).attr('id');
     $('.windowSelector button').removeClass('active');
     $(el).addClass('active');
@@ -137,7 +162,8 @@ $('.generate').on('click', function () {
 
     let activeWindowType = $('.windowSelector button.active').attr('id');
     console.log(`activeWindowType ${activeWindowType}`);
-    if (activeWindowType !== "none") {
+    if(activeWindowType !== "none")
+    {
         console.log('generate window');
         addWindow(activeWindowType);
     }
@@ -146,12 +172,13 @@ $('.generate').on('click', function () {
 $('.export').on('click', function () {
     let downloadLink = document.createElement('a');
     downloadLink.download = 'box.svg';
-    downloadLink.href = "data:image/svg+xml," + (new XMLSerializer).serializeToString($('.box svg')[0]);
+    downloadLink.href = "data:image/svg+xml," + (new XMLSerializer).serializeToString($('#box svg')[0]);
     console.log(downloadLink.href);
     downloadLink.click();
 });
 
-function parseSTL() {
+function parseSTL()
+{
     console.log('parseSTL');
     let stlFile = $('#stlFile');
     let file = stlFile[0].files[0];
@@ -160,19 +187,20 @@ function parseSTL() {
     let fileExt = fileName.substr(fileName.lastIndexOf('.') + 1).toLowerCase();
     console.log(`fileExt: ${fileExt}`);
 
-    if (fileExt === "stl") {
+    if(fileExt === "stl")
+    {
         let formData = new FormData();
         formData.append('file', file);
 
         $.ajax({
-            type: 'POST',
-            url: '/parseSTL',
-            data: formData,
+            type:        'POST',
+            url:         '/parseSTL',
+            data:        formData,
             contentType: false,
             processData: false,
-            async: false,
-            cache: false,
-            success: function (data) {
+            async:       false,
+            cache:       false,
+            success:     function (data) {
                 console.log(data);
                 // set width, height, and depth values
                 $('#width').val(data[0]);
@@ -181,20 +209,24 @@ function parseSTL() {
                 $('.generate').click();
             }
         });
-    } else {
+    }
+    else
+    {
         window.alert('Please select a valid .stl file');
         stlFile.val('');
     }
 }
 
-function updateBox(newSVG = null) {
-    if(newSVG === null) {
+function updateBox(newSVG = null)
+{
+    if(newSVG === null)
+    {
         newSVG = generateBox()
     }
     box.empty();
     box.append(newSVG);
 }
 
-$(function() {
+$(function () {
     box.append(generateBox());
 });
